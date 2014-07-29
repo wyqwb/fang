0d33912d5aa12d39467891a0d8a645de155b403e
zhangkai <cong_rong520@163.com>
2014-07-29 14:38:26 +0800

    update

---
+++ application/logs/log-2014-07-28.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-07-28 13:58:39 --> 404 Page Not Found --> favicon.ico
+ERROR - 2014-07-28 14:16:21 --> 404 Page Not Found --> favicon.ico
