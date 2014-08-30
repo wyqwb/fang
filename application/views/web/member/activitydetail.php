a2ecdafc947ead962bd6751a3896c56c7a733cc7
zhangkai <cong_rong520@163.com>
2014-08-30 09:56:37 +0800

    uip

---
+++ application/views/web/member/activitydetail.php
@@ -0,0 +1,20 @@
+﻿        <div class="content fr">
+            <!-- 活动专区>详细 -->
+            <div class="ui-winbox2 mt20">
+                <div class="hd clearfix">
+                    <h3 class="ui-titSty1 fl mr20"><span>活动专区</span></h3>
+                </div>
+                <div class="bd clearfix">
+                    <div class="myArticle">
+                        <div class="myArticle-hd tac">
+                            <h1 class="fw400 fs14 c_ad0909"><?php echo $detail['title'];?></h1>
+                            <p>&nbsp;<span>发布时间：<?php echo $detail['published'];?></span>&nbsp;</p>
+                        </div>
+                        <div class="myArticle-bd">
+                        <?php echo $detail['content'];?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
