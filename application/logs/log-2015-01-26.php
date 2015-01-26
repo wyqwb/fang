12ad35b1e4ad31043b2e2b53b2dd4c26850f72c4
wyqwb <751239183@qq,com>
2015-01-26 17:21:01 +0800

    server master commit

---
+++ application/logs/log-2015-01-26.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-01-26 17:20:17 --> Severity: Notice  --> Undefined index: HTTP_ACCEPT_LANGUAGE /sda1/www/html/application/config/config.php 82
+ERROR - 2015-01-26 17:20:17 --> Severity: Warning  --> mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: Can't connect to local MySQL server through socket '/var/lib/mysql/mysql.sock' (2) /sda1/www/html/system/database/drivers/mysql/mysql_driver.php 73
+ERROR - 2015-01-26 17:20:17 --> Unable to connect to the database
