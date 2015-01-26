d2b0adea525f657861536d548afcd85f726a03d6
wyqwb <751239183@qq,com>
2015-01-26 17:30:14 +0800

    server local first

--- application/logs/log-2015-01-26.php
+++ application/logs/log-2015-01-26.php
@@ -3,3 +3,7 @@
 ERROR - 2015-01-26 17:20:17 --> Severity: Notice  --> Undefined index: HTTP_ACCEPT_LANGUAGE /sda1/www/html/application/config/config.php 82
 ERROR - 2015-01-26 17:20:17 --> Severity: Warning  --> mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: Can't connect to local MySQL server through socket '/var/lib/mysql/mysql.sock' (2) /sda1/www/html/system/database/drivers/mysql/mysql_driver.php 73
 ERROR - 2015-01-26 17:20:17 --> Unable to connect to the database
+ERROR - 2015-01-26 17:21:05 --> Severity: Warning  --> mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: Can't connect to local MySQL server through socket '/var/lib/mysql/mysql.sock' (2) /sda1/www/html/system/database/drivers/mysql/mysql_driver.php 73
+ERROR - 2015-01-26 17:21:05 --> Unable to connect to the database
+ERROR - 2015-01-26 17:21:24 --> Severity: Warning  --> mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: Can't connect to local MySQL server through socket '/var/lib/mysql/mysql.sock' (2) /sda1/www/html/system/database/drivers/mysql/mysql_driver.php 73
+ERROR - 2015-01-26 17:21:24 --> Unable to connect to the database
